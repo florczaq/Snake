4741f0b7787a31286946ce9475aaab9a2696f01c
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2022-02-24 22:55:16 +0100

    skins

--- Game.cpp
+++ Game.cpp
@@ -33,12 +33,18 @@ void Game::render()
 	window->clear(sf::Color(16, 01, 16));
 	window->draw(gameScore);
 	window->draw(signature);
-	if (!paused && !end) window->draw(pauseButton);
+
+	if (!paused && !end && !picking) {
+		window->draw(pauseButton);
+		window->draw(skinPickerButton);
+	}
 	window->draw(snakeBody);
 	window->draw(snakeHead);
 	window->draw(point);
+
 	if (paused) window->draw(pauseMenu);
 	if (end) window->draw(endMenu);
+	if (picking) window->draw(skinPicker);
 
 	window->display();
 }
@@ -47,6 +53,7 @@ void Game::snakeMove(sf::Clock& frameTime)
 {
 	snakeHead.move();
 	snakeBody.afterMove(snakeHead.get_pos());
+	skins.set_rainbow_color(snakeHead, snakeBody);
 	frameTime.restart();
 	if (point.collision(snakeHead.get_shape().getGlobalBounds())) { // add func get bounds in SnakeHead
 		point.pick_new_position(snakeBody.get_shapes(), snakeHead.get_shape().getSize().x);
@@ -62,7 +69,7 @@ void Game::snakeMove(sf::Clock& frameTime)
 
 void Game::update(sf::Clock& frameTime) {
 	events(frameTime);
-	if (frameTime.getElapsedTime().asMilliseconds() >= 70 * difficulty && !paused && !end)
+	if (frameTime.getElapsedTime().asMilliseconds() >= 70 * difficulty && !paused && !end && !picking)
 		snakeMove(frameTime);
 }
 void Game::initVars(double width, double height) {
@@ -86,8 +93,8 @@ void Game::initVars(double width, double height) {
 	gameScore = Game_score(pixelFont, width, height);
 	signature = Signature(signFont, width, height);
 	pauseButton = Button(pixelFont, width * 0.03, height * 0.055, "||", sf::Color::Transparent, sf::Vector2f(width * 0.95, height * 0.05), 31, sf::Color(255, 255, 255, 30));
-	skinPickerButton = Button(pixelFont, width, height, "S", sf::Color(255,255,255,30), sf::Vector2f(width*0.85, height*0.05));
-	skinPicker = SkinPicker(width/2, height*0.8, sf::Color(61, 21, 61), sf::Vector2f(width/2, height/2), font, textures);
+	skinPickerButton = Button(pixelFont, width * 0.03, height * 0.055, "s", sf::Color::Transparent, sf::Vector2f(width * 0.9, height * 0.045), 41, sf::Color(255, 255, 255, 30));
+	skinPicker = SkinPicker(width / 2, height * 0.8, sf::Color(61, 21, 61), sf::Vector2f(width / 2, height / 2), font, textures);
 }
 
 void Game::events(sf::Clock& frameTime)
@@ -113,15 +120,32 @@ void Game::events(sf::Clock& frameTime)
 		case sf::Event::MouseMoved:
 			mouseCursor.set_position(sf::Vector2f(sf::Mouse::getPosition(*window)));
 			if (paused) pauseMenu.hover(mouseCursor.get_bounds());
-			else if (end) endMenu.hover(mouseCursor.get_bounds());
+			else if (end) endMenu.hover(mouseCursor.get_bounds()); \
+			else if (picking) skinPicker.hover(mouseCursor.get_bounds(), false);
 			break;
 
 		case sf::Event::MouseButtonPressed:
 
 			if (event.mouseButton.button == sf::Mouse::Left) {
-				if ((pauseMenu.pressed(mouseCursor.get_bounds(), paused) == 2 && paused) || (endMenu.pressed(mouseCursor.get_bounds(), paused) == 2 && end)) {
-					result = 1;
-					window->close();
+
+				if (!picking) {
+					if ((pauseMenu.pressed(mouseCursor.get_bounds(), paused) == 2 && paused) || (endMenu.pressed(mouseCursor.get_bounds(), paused) == 2 && end)) {
+						result = 1;
+						window->close();
+					}
+				}
+				else {
+					int* temp = new int(skinPicker.hover(mouseCursor.get_bounds(), true));
+					if (*temp != - 1) {
+						skins.setSkin(*temp);
+						skins.updateSkin(snakeHead, snakeBody);
+						picking = false;
+					}
+					delete temp;
+				}
+
+				if (mouseCursor.pressed(skinPickerButton.get_bounds())) {
+					picking = true;
 				}
 
 				if (mouseCursor.pressed(pauseButton.get_bounds())) {
@@ -135,9 +159,6 @@ void Game::events(sf::Clock& frameTime)
 
 			if (!paused && !end && snakeHead.changeDirection(event.key.code))
 				snakeMove(frameTime);
-
-
-
 			break;
 		}
 	}
